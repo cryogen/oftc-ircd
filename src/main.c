30c023ace4e5ec476674dd66a62ee963aeaa0f37
Stuart Walsh <stu@ipng.org.uk>
2014-04-16 08:45:24 +0100

    more framework stuff

--- src/main.c
+++ src/main.c
@@ -1,8 +1,63 @@
-/* BSD License blah blah
- * main.c - Main Stuff
+/*
+ * Copyright (c) 2014, Stuart Walsh
+ * All rights reserved.
+ * main.c main startup functions
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-int main()
+#include <uv.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void
+process_commandline(char *const *args, int arg_count)
 {
+  int opt;
+
+  while((opt = getopt(arg_count, args, "c:")) != -1)
+  {
+    switch(opt)
+    {
+      case 'c':
+        break;
+
+      default:
+        fprintf(stderr, "Unknown option: %c", opt);
+        exit(EXIT_FAILURE);
+        break;
+    }
+  }
+}
+
+int main(char *argv[], int argc)
+{
+  uv_loop_t *uv_loop;
+
+  process_commandline(argv, argc);
+
+  uv_loop = uv_default_loop();
+
+  uv_run(uv_loop, UV_RUN_ONCE);
+
   return 0;
 }
