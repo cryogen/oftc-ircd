b30d5efdd683ab1efebb4e22865e91226b6f7eae
Stuart Walsh <stu@ipng.org.uk>
2014-11-02 17:42:24 +0000

    identify unit tests too

--- tests/hash_tests.c
+++ tests/hash_tests.c
@@ -1,12 +1,38 @@
-#include <check.h>
-#include "hash.h"
+/*
+ * Copyright (c) 2014, Stuart Walsh
+ * All rights reserved.
+ * hash_tests.c unit tests for hashing
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, thi
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" A
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE F
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGE
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <check.h>
 #include <stdlib.h>
 
-void 
+#include "hash.h"
+
+void
 setup()
 {
-  hash_init();
+    hash_init();
 }
 
 void
@@ -16,62 +42,62 @@ cleanup()
 
 START_TEST(hash_new_WhenCalledWithNameAndLenReturnsHash)
 {
-  Hash *h = hash_new("Test hash", HASHLEN);
+    Hash *h = hash_new("Test hash", HASHLEN);
 
-  ck_assert(h != NULL);
-  ck_assert(h->Name != NULL);
-  ck_assert_str_eq(h->Name, "Test hash");
+    ck_assert(h != NULL);
+    ck_assert(h->Name != NULL);
+    ck_assert_str_eq(h->Name, "Test hash");
 }
 END_TEST
 
 START_TEST(hash_new_WhenCalledWithNullNameAndLenReturnsHash)
 {
-  Hash *h = hash_new(NULL, HASHLEN);
+    Hash *h = hash_new(NULL, HASHLEN);
 
-  ck_assert(h != NULL);
+    ck_assert(h != NULL);
 }
 END_TEST
 
 START_TEST(hash_new_WhenCalledWithZeroLenReturnsNull)
 {
-  Hash *h = hash_new("Fail", 0);
+    Hash *h = hash_new("Fail", 0);
 
-  ck_assert(h == NULL);
+    ck_assert(h == NULL);
 }
 END_TEST
 
 Suite *
 hash_suite()
 {
-  Suite  *s;
-  TCase *tcCore;
-  
-  s = suite_create("Hash");
+    Suite  *s;
+    TCase *tcCore;
 
-  tcCore = tcase_create("Core");
+    s = suite_create("Hash");
 
-  tcase_add_unchecked_fixture(tcCore, setup, cleanup);
-  tcase_add_test(tcCore, hash_new_WhenCalledWithNameAndLenReturnsHash);
-  tcase_add_test(tcCore, hash_new_WhenCalledWithNullNameAndLenReturnsHash);
-  tcase_add_test(tcCore, hash_new_WhenCalledWithZeroLenReturnsNull);
-  suite_add_tcase(s, tcCore);
+    tcCore = tcase_create("Core");
 
-  return s;
+    tcase_add_unchecked_fixture(tcCore, setup, cleanup);
+    tcase_add_test(tcCore, hash_new_WhenCalledWithNameAndLenReturnsHash);
+    tcase_add_test(tcCore, hash_new_WhenCalledWithNullNameAndLenReturnsHash);
+    tcase_add_test(tcCore, hash_new_WhenCalledWithZeroLenReturnsNull);
+    suite_add_tcase(s, tcCore);
+
+    return s;
 }
 
-int 
+int
 main()
 {
-  int numberFailed;
-  Suite *s;
-  SRunner *sr;
+    int numberFailed;
+    Suite *s;
+    SRunner *sr;
 
-  s = hash_suite();
-  sr = srunner_create(s);
+    s = hash_suite();
+    sr = srunner_create(s);
 
-  srunner_run_all(sr, CK_NORMAL);
-  numberFailed = srunner_ntests_failed(sr);
-  srunner_free(sr);
-
-  return (numberFailed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+    srunner_run_all(sr, CK_NORMAL);
+    numberFailed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    
+    return (numberFailed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
