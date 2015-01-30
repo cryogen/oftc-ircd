5d4bf0c21fa4524cd5791e051537683e4563bda8
Stuart Walsh <stu@ipng.org.uk>
2015-01-30 18:48:43 +0000

    refactor vector tests

--- tests/vector_tests.c
+++ tests/vector_tests.c
@@ -24,234 +24,304 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <check.h>
+#include "opmock.h"
 
 #include "vector.h"
+#include "memory_stub.h"
 
-static void
-setup()
-{
-}
-
-static void
-cleanup()
-{
-}
-
-START_TEST(vector_new_WhenCalledWithZeroElementSizeReturnsNull)
+void
+vector_new_when_called_with_zero_element_size_returns_null()
 {
     Vector *vector;
 
     vector = vector_new(0, 0);
 
-    ck_assert(vector == NULL);
+    OP_ASSERT_TRUE(vector == NULL);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_new_WhenCalledWithZeroCapacitySetsDefaultCapacity)
+void
+vector_new_when_called_with_zero_capacity_sets_default_capacity()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096];
+
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
 
     vector = vector_new(0, 4);
 
-    ck_assert(vector != NULL);
-    ck_assert_int_eq(vector->Capacity, DEFAULT_VECTOR_CAPACITY);
+    OP_ASSERT_TRUE(vector != NULL);
+    OP_ASSERT_EQUAL_INT(DEFAULT_VECTOR_CAPACITY, (int)vector->Capacity);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_new_WhenCalledSetsCapactityAndElementSize)
+void
+vector_new_when_called_sets_capactity_and_element_size()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096];
+
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
 
     vector = vector_new(100, 4);
 
-    ck_assert(vector != NULL);
-    ck_assert_int_eq(vector->Capacity, 100);
-    ck_assert_int_eq(vector->ElementSize, 4);
+    OP_ASSERT_TRUE(vector != NULL);
+    OP_ASSERT_EQUAL_INT(100, (int)vector->Capacity);
+    OP_ASSERT_EQUAL_INT(4, (int)vector->ElementSize);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_push_back_WhenCalledWithNullThisReturnsNull)
+void
+vector_push_back_when_called_with_null_this_returns_null()
 {
     void  *ret = vector_push_back(NULL, NULL);
 
-    ck_assert(ret == NULL);
+    OP_ASSERT_TRUE(ret == NULL);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_push_back_WhenCalledWithNullValueReturnsNull)
+void
+vector_push_back_when_called_with_null_value_returns_null()
 {
     Vector *vector;
     void *ptr;
+    Vector retVector = { 0 };
+    char data[4096];
+
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
 
     vector = vector_new(100, 4);
 
     ptr = vector_push_back(vector, NULL);
 
-    ck_assert(ptr == NULL);
+    OP_ASSERT_TRUE(ptr == NULL);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_push_back_WhenCalledSetsLength)
+void
+vector_push_back_when_called_sets_length()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096];
+
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
 
     vector = vector_new(100, 4);
 
     vector_push_back(vector, "TEST");
 
-    ck_assert_int_eq(vector->Length, 1);
+    OP_ASSERT_EQUAL_INT(1, (int)vector->Length);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_push_back_WhenCalledDoesNotIncreaseCapacity)
+void
+vector_push_back_when_called_does_not_increase_capacity()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096];
+
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
 
     vector = vector_new(100, 4);
 
     vector_push_back(vector, "TEST");
 
-    ck_assert_int_eq(vector->Capacity, 100);
+    OP_ASSERT_EQUAL_INT(100, (int)vector->Capacity);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_push_back_WhenCalledPutsValueInData)
+void
+vector_push_back_when_called_puts_value_in_data()
 {
     Vector *vector;
     void *ret;
+    Vector retVector = { 0 };
+    char data[4096] = { 0 };
+
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
 
     vector = vector_new(100, 4);
 
     ret = vector_push_back(vector, "TEST");
 
-    ck_assert_str_eq(vector->Data, "TEST");
-    ck_assert_str_eq(ret, "TEST");
+    OP_ASSERT_EQUAL_CSTRING("TEST", vector->Data);
+    OP_ASSERT_EQUAL_CSTRING("TEST", ret);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_push_back_WhenCalledTwicePutsValueInDataAndSetsLength)
+void vector_push_back_when_called_twice_puts_value_in_data_and_sets_length()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096] = { 0 };
 
-    vector = vector_new(100, 4);
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
+
+    vector = vector_new(100, 5);
 
     vector_push_back(vector, "TEST");
     vector_push_back(vector, "1TES");
 
-    ck_assert(memcmp(vector->Data, "TEST", 4) == 0);
-    ck_assert(memcmp(vector->Data + 4, "1TES", 4) == 0);
-    ck_assert_int_eq(vector->Length, 2);
-    ck_assert_int_eq(vector->Capacity, 100);
+    OP_ASSERT_EQUAL_CSTRING("TEST", vector->Data);
+    OP_ASSERT_EQUAL_CSTRING("1TES", vector->Data + 5);
+    OP_ASSERT_EQUAL_INT(2, (int)vector->Length);
+    OP_ASSERT_EQUAL_INT(100, (int)vector->Capacity);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_push_back_WhenAboveCapacityExtendsVector)
+void
+vector_push_back_when_above_capacity_extends_vector()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096] = { 0 };
 
-    vector = vector_new(1, 4);
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
+    Realloc_ExpectAndReturn(NULL, 0, &data, NULL, NULL);
+
+    vector = vector_new(1, 5);
 
     vector_push_back(vector, "TEST");
 
-    ck_assert_int_eq(vector->Capacity, 1);
+    OP_ASSERT_EQUAL_INT(1, (int)vector->Capacity);
 
     vector_push_back(vector, "1TES");
 
-    ck_assert(memcmp(vector->Data, "TEST", 4) == 0);
-    ck_assert(memcmp(vector->Data + 4, "1TES", 4) == 0);
-    ck_assert_int_eq(vector->Length, 2);
-    ck_assert_int_eq(vector->Capacity, 2);
+    OP_ASSERT_EQUAL_CSTRING("TEST", vector->Data);
+    OP_ASSERT_EQUAL_CSTRING("1TES", vector->Data + 5);
+    OP_ASSERT_EQUAL_INT(2, (int)vector->Length);
+    OP_ASSERT_EQUAL_INT(2, (int)vector->Capacity);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_length_WhenCalledWithNullThisReturnsZero)
+void
+vector_length_when_called_with_null_this_returns_zero()
 {
-    ck_assert_int_eq(vector_length(NULL), 0);
+    OP_ASSERT_EQUAL_INT(0, (int)vector_length(NULL));
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_length_WhenCalledReturnsCorrectLength)
+void
+vector_length_when_called_returns_correct_length()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096] = { 0 };
+
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
 
     vector = vector_new(0, 4);
 
     vector_push_back(vector, "TEST");
 
-    ck_assert_int_eq(vector_length(vector), 1);
-    ck_assert_int_eq(vector_length(vector), vector->Length);
+    OP_ASSERT_EQUAL_INT(1, (int)vector_length(vector));
+    OP_ASSERT_EQUAL_INT((int)vector->Length, (int)vector_length(vector));
 
     vector_push_back(vector, "1TES");
 
-    ck_assert_int_eq(vector_length(vector), 2);
-    ck_assert_int_eq(vector_length(vector), vector->Length);
+    OP_ASSERT_EQUAL_INT(2, (int)vector_length(vector));
+    OP_ASSERT_EQUAL_INT((int)vector->Length, (int)vector_length(vector));
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_get_WhenCalledWithNullThisReturnsNull)
+void
+vector_get_when_called_with_null_this_returns_null()
 {
-    ck_assert(vector_get(NULL, 0) == NULL);
+    OP_ASSERT_TRUE(vector_get(NULL, 0) == NULL);
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_get_WhenCalledReturnsCorrectValue)
+void
+vector_get_when_called_returns_correct_value()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096] = { 0 };
 
-    vector = vector_new(0, 4);
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
+
+    vector = vector_new(0, 5);
 
     vector_push_back(vector, "TEST");
     vector_push_back(vector, "1TES");
 
-    ck_assert(memcmp(vector_get(vector, 0), "TEST", 4) == 0);
-    ck_assert_str_eq(vector_get(vector, 1), "1TES");
+    OP_ASSERT_EQUAL_CSTRING("TEST", vector_get(vector, 0));
+    OP_ASSERT_EQUAL_CSTRING("1TES", vector_get(vector, 1));
+    OP_VERIFY();
 }
-END_TEST
 
-START_TEST(vector_get_WhenCalledWithOutOfRangeIndexReturnsNull)
+void
+vector_get_when_called_with_out_of_range_index_returns_null()
 {
     Vector *vector;
+    Vector retVector = { 0 };
+    char data[4096] = { 0 };
 
-    vector = vector_new(0, 4);
+    Malloc_ExpectAndReturn(0, &retVector, NULL);
+    Malloc_ExpectAndReturn(0, data, NULL);
+
+    vector = vector_new(0, 5);
 
     vector_push_back(vector, "TEST");
     vector_push_back(vector, "1TES");
 
-    ck_assert(vector_get(vector, 2) == NULL);
-    ck_assert_str_eq(vector_get(vector, 1), "1TES");
+    OP_ASSERT_TRUE(vector_get(vector, 2) == NULL);
+    OP_ASSERT_EQUAL_CSTRING("TEST", vector_get(vector, 0));
+    OP_ASSERT_EQUAL_CSTRING("1TES", vector_get(vector, 1));
+    OP_VERIFY();
 }
-END_TEST
 
-Suite *
-vector_suite()
+int
+main()
 {
-    Suite *s;
-    TCase *tcCore;
+    opmock_test_suite_reset();
 
-    s = suite_create("vector");
+    opmock_register_test(vector_new_when_called_with_zero_element_size_returns_null,
+                         "vector_new_when_called_with_zero_element_size_returns_null");
+    opmock_register_test(vector_new_when_called_with_zero_capacity_sets_default_capacity,
+                         "vector_new_when_called_with_zero_capacity_sets_default_capacity");
+    opmock_register_test(vector_new_when_called_sets_capactity_and_element_size,
+                         "vector_new_when_called_sets_capactity_and_element_size");
+    opmock_register_test(vector_push_back_when_called_with_null_this_returns_null,
+                         "vector_push_back_when_called_with_null_this_returns_null");
+    opmock_register_test(vector_push_back_when_called_with_null_value_returns_null,
+                         "vector_push_back_when_called_with_null_value_returns_null");
+    opmock_register_test(vector_push_back_when_called_sets_length,
+                         "vector_push_back_when_called_sets_length");
+    opmock_register_test(vector_push_back_when_called_does_not_increase_capacity,
+                         "vector_push_back_when_called_does_not_increase_capacity");
+    opmock_register_test(vector_push_back_when_called_puts_value_in_data,
+                         "vector_push_back_when_called_puts_value_in_data");
+    opmock_register_test(vector_push_back_when_called_twice_puts_value_in_data_and_sets_length,
+                         "vector_push_back_when_called_twice_puts_value_in_data_and_sets_length");
+    opmock_register_test(vector_push_back_when_above_capacity_extends_vector,
+                         "vector_push_back_when_above_capacity_extends_vector");
+    opmock_register_test(vector_length_when_called_with_null_this_returns_zero,
+                         "vector_length_when_called_with_null_this_returns_zero");
+    opmock_register_test(vector_length_when_called_returns_correct_length,
+                         "vector_length_when_called_returns_correct_length");
+    opmock_register_test(vector_get_when_called_with_null_this_returns_null,
+                         "vector_get_when_called_with_null_this_returns_null");
+    opmock_register_test(vector_get_when_called_returns_correct_value,
+                         "vector_get_when_called_returns_correct_value");
+    opmock_register_test(vector_get_when_called_with_out_of_range_index_returns_null,
+                         "vector_get_when_called_with_out_of_range_index_returns_null");
 
-    tcCore = tcase_create("Core");
+    opmock_test_suite_run();
 
-    tcase_add_unchecked_fixture(tcCore, setup, cleanup);
-
-    tcase_add_test(tcCore, vector_new_WhenCalledWithZeroElementSizeReturnsNull);
-    tcase_add_test(tcCore, vector_new_WhenCalledWithZeroCapacitySetsDefaultCapacity);
-    tcase_add_test(tcCore, vector_new_WhenCalledSetsCapactityAndElementSize);
-    tcase_add_test(tcCore, vector_push_back_WhenCalledWithNullThisReturnsNull);
-    tcase_add_test(tcCore, vector_push_back_WhenCalledWithNullValueReturnsNull);
-    tcase_add_test(tcCore, vector_push_back_WhenCalledSetsLength);
-    tcase_add_test(tcCore, vector_push_back_WhenCalledDoesNotIncreaseCapacity);
-    tcase_add_test(tcCore, vector_push_back_WhenCalledPutsValueInData);
-    tcase_add_test(tcCore, vector_push_back_WhenCalledTwicePutsValueInDataAndSetsLength);
-    tcase_add_test(tcCore, vector_push_back_WhenAboveCapacityExtendsVector);
-    tcase_add_test(tcCore, vector_length_WhenCalledWithNullThisReturnsZero);
-    tcase_add_test(tcCore, vector_length_WhenCalledReturnsCorrectLength);
-    tcase_add_test(tcCore, vector_get_WhenCalledWithNullThisReturnsNull);
-    tcase_add_test(tcCore, vector_get_WhenCalledReturnsCorrectValue);
-    tcase_add_test(tcCore, vector_get_WhenCalledWithOutOfRangeIndexReturnsNull);
-
-    suite_add_tcase(s, tcCore);
-    
-    return s;
+    return opmock_test_error > 0;
 }
